264ffb6b70abc7300fd1d0706df7c09cddee07c5
Ritesh Maurya <riteshkumar411552@gmail.com>
2023-07-20 09:52:16 +0530

    added router and controller for deleting a post

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,4 +1,6 @@
 const Post = require('../models/post');
+const Comment = require('../models/comment');
+
 module.exports.create = async (req, res) => {
  await Post.create({
     content:req.body.content,
@@ -6,4 +8,18 @@ module.exports.create = async (req, res) => {
  });
 
  return res.redirect('back');
+}
+
+
+module.exports.destroy = async (req, res) => {
+   const post = await Post.findById(req.params.id);
+   console.log(post);
+   if(post.user == req.user.id) {
+      post.remove();
+      await Comment.deleteMany({post:req.params.id});
+      return res.redirect('back');
+
+   } else {
+      return res.redirect('back');
+   }
 }
