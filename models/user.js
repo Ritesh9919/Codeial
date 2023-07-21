c7b62c7221268a5f43bb18d5b870125d01239201
Ritesh Maurya <riteshkumar411552@gmail.com>
2023-07-21 13:54:31 +0530

    Setted multer for users avatar

--- models/user.js
+++ models/user.js
@@ -21,5 +21,16 @@ const userSchema = new mongoose.Schema({
     }
 },{timestamps:true});
 
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, path.join(__dirname, '..', AVATAR_PATH));
+    },
+    filename: function (req, file, cb) {
+      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+      cb(null, file.fieldname + '-' + uniqueSuffix)
+    }
+  })
+
 const User = mongoose.model('User', userSchema);
 module.exports = User;
