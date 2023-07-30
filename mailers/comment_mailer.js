4d9fab9ca40a9e33afb4d48d7dad97bee8e83271
Ritesh Maurya <riteshkumar411552@gmail.com>
2023-07-30 12:59:15 +0530

    sended html template email

--- mailers/comment_mailer.js
+++ mailers/comment_mailer.js
@@ -2,11 +2,12 @@ const nodemailer = require('../config/nodemailer');
 
 
 module.exports.newComment = (comment) => {
+    const htmlString = nodemailer.renderTemplate({comment:comment}, '/comments/new_comment.ejs');
     nodemailer.transporter.sendMail({
         from:'riteshkumar411552@gmail.com',
         to: comment.user.email,
         subject:'new comment publised',
-        html:'Your comment now publiced!'
+        html:htmlString
     }, (err, info) => {
         if(err) {console.log('error in sending mail', err); return};
         console.log('Message sent', info);
